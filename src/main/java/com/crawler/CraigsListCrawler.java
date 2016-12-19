97af15a1598aa4dc44b3a908c5de853f20f46748
urbo143851 <urbo143851@gmail.com>
2016-12-18 19:13:02 -0600

    make sure there are enough pages to query

--- src/main/java/com/crawler/CraigsListCrawler.java
+++ src/main/java/com/crawler/CraigsListCrawler.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlListItem;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -17,19 +18,24 @@ public class CraigsListCrawler implements Crawler{
 	
 	WebClient client = new WebClient(BrowserVersion.CHROME); 
 	
+	boolean enoughPages;
+	
 	public void execute(String customSearchQuery, String pagesToQuery){
 		try {
-			run(customSearchQuery);
-			for (int i = 1; i < Integer.valueOf(pagesToQuery); i++){
-				run(customSearchQuery, String.valueOf(i));
+			runPrimary(customSearchQuery, pagesToQuery);
+			if (enoughPages){
+				for (int i = 1; i < Integer.valueOf(pagesToQuery); i++){
+					run(customSearchQuery, String.valueOf(i));
+				}
 			}
+					
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 		
 	@SuppressWarnings("unchecked")
-	public void run(String customSearchQuery) throws IOException{
+	public void runPrimary(String customSearchQuery, String pagesToQuery) throws IOException{
 		FileWriter fileWriter = new FileWriter("craigsfile.txt");	
 		client.getOptions().setCssEnabled(false);
 		client.getOptions().setJavaScriptEnabled(false);
@@ -37,7 +43,9 @@ public class CraigsListCrawler implements Crawler{
 		try {  
 		  String searchUrl = "https://newyork.craigslist.org/search/sss?sort=rel&query=" + URLEncoder.encode(customSearchQuery, "UTF-8");
 		  HtmlPage page = client.getPage(searchUrl);
-		  
+		 
+		  enoughPages = validate(page,pagesToQuery);
+	
 		  List<HtmlListItem> sortableResultsUnorderedList = (List<HtmlListItem>) page.getByXPath("//*[@id='sortable-results']/ul/li");
 		  if(sortableResultsUnorderedList.isEmpty()){  
 		  		System.out.println("No items found !");
@@ -75,7 +83,6 @@ public class CraigsListCrawler implements Crawler{
 				  			 URLEncoder.encode(customSearchQuery, "UTF-8");
 		  
 		  HtmlPage page = client.getPage(searchUrl);
-		  
 		  List<HtmlListItem> sortableResultsUnorderedList = (List<HtmlListItem>) page.getByXPath("//*[@id='sortable-results']/ul/li");
 		  if(sortableResultsUnorderedList.isEmpty()){  
 		  		System.out.println("No items found !");
@@ -86,6 +93,7 @@ public class CraigsListCrawler implements Crawler{
 		  			System.out.println(description.asText());
 		  			fileWriter.write(description.asText());
 		  			fileWriter.write(" at price: ");
+		  			
 		  			HtmlAnchor price = (HtmlAnchor)item.getFirstByXPath(".//a");
 		  			System.out.println(price.asText());
 		  			fileWriter.write(price.asText());
@@ -97,6 +105,15 @@ public class CraigsListCrawler implements Crawler{
 		}finally{
 			fileWriter.close();
 		}
+	}
+	
+	private boolean validate(HtmlPage page, String pagesToQuery){
+		HtmlElement availablePages =  (HtmlElement) page.getFirstByXPath("//*[@id='searchform']/div[5]/div[3]/span[2]/span[3]/span[2]");
+	    int numPages = Integer.valueOf(availablePages.asText())/100;
+	    if (numPages >= Integer.valueOf(pagesToQuery)){
+	    	  return true;
+	    }
+		return false;  	  
 	}
 }
 
