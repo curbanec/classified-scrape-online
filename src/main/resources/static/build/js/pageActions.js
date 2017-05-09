3f35a5913c1a43060146c337cbbe68f5a383b901
urbo143851 <urbo143851@gmail.com>
2017-05-08 19:53:33 -0500

    add table row onSubmit

--- src/main/resources/static/build/js/pageActions.js
+++ src/main/resources/static/build/js/pageActions.js
@@ -2,7 +2,6 @@ $.getScript("/js/regionsForStates.js", function(data, textStatus, jqxhr) {
 	console.log('Load was performed for state dropdown.');
 	});
 
-
 $("#maxDepth").on("click", disable);
 $("#alertForm").on("click", addToSearchList);
 
@@ -20,8 +19,19 @@ function disable(event) {
 }
 
 function addToSearchList(){
-	var li = document.createElement("LI"); 
+	/*var li = document.createElement("LI"); 
 	li.innerHTML = "hey";
 	var input = document.getElementById("alertsSection");
-    document.getElementById("alertsSection").appendChild(li);
+    document.getElementById("alertsSection").appendChild(li);*/
+    
+    var table = document.getElementById("tableBody");
+    var row = table.insertRow(0);
+   
+    var cell2 = row.insertCell(1);
+    
+    //http://api.jquery.com/append/
+    
+    cell1.innerHTML = "<td class="a-center "><input type="checkbox" class="flat" name="table_records"></td>";
+    cell2.innerHTML = "NEW CELL2";
+    
 }
