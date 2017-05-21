98d50bfcbe1113953df6b98356dc3153e5d771e8
urbo143851 <urbo143851@gmail.com>
2017-05-21 17:06:10 -0500

    move 2 js

--- src/main/resources/static/js/pageActions.js
+++ src/main/resources/static/js/pageActions.js
@@ -1,12 +1,33 @@
 $.getScript("/js/regionsForStates.js", function(data, textStatus, jqxhr) {
 	console.log('Load was performed for state dropdown.');
 	});
-
+$.getScript("/js/dateFormat.js", function(data, textStatus, jqxhr) {
+	console.log('Load was performed for dateFormat.');
+	});
 
 $("#maxDepth").on("click", disable);
 $("#alertForm").on("click", addToSearchList);
 
 count = 0;
+
+
+
+
+function cancel(queryId){
+	
+	$.ajax({
+		url:'/api/main/cancel?queryId='+queryId, 
+		type:'GET',
+		/*success:function(data){
+			stuffs=data;
+			setupGraph(data);
+		}*/
+	});
+	
+	
+	
+	
+}
 
 function disable(event) {
 	count++;
@@ -20,21 +41,95 @@ function disable(event) {
 }
 
 function addToSearchList(){
-	var li = document.createElement("LI"); 
-	li.innerHTML = "hey";
-	var input = document.getElementById("addToSearchList");
-    document.getElementById("addToSearchList").appendChild(li);
-}
+	
+	var area = $('#area').val();
+	var submissionTime = new Date(); // May 23, 2014 11:47:56 PM
+	var submissionTimeDate = dateFormat(submissionTime, "yyyy-mm-d HH:MM");
+	console.log(submissionTimeDate);
+	var queryId = Math.floor((Math.random() * 1000000) + 1);
+	// var listenerDuration = '';
+	var query = $('#query').val();
+	var notifyAddress = $('#notifyAddress').val();
+	// var active = '';
+	
+	/*var secs = 60 * 1000;
+	setInterval(function() {
+	    var $badge = $('#nhb_01');
+	    $badge.text((parseFloat($badge.text())+0.17).toFixed(2));
+	}, secs);*/
+	
+	var json = {"submissionTimeDate":submissionTimeDate, "area":area, "query":query, "notifyAddress":notifyAddress, "queryId":queryId};
+	
+	$('#tableBody').append('<tr class="odd pointer"> <td class="a-center "> <input type="checkbox" class="flat" name="table_records"> </td>  <td class=" ">'+ area +'</td><td class=" ">'+ submissionTime +'</td><td class=" ">1</td><td class=" ">'+ query +'</td><td class=" ">'+ notifyAddress +'</td><td class="a-right a-right ">true</td>      <td class="a-right a-right ">   <button type="button" onclick="cancel('+ queryId +')"   >'+ queryId +'</button>           </td>                                                                       </tr>');
+	    if ($("input.flat")[0]) {
+	        $(document).ready(function () {
+	            $('input.flat').iCheck({
+	                checkboxClass: 'icheckbox_flat-green',
+	                radioClass: 'iradio_flat-green'
+	            });
+	        });
+	    }
+	    $('table input').on('ifChecked', function () {
+	        checkState = '';
+	        $(this).parent().parent().parent().addClass('selected');
+	        countChecked();
+	    });
+	    $('table input').on('ifUnchecked', function () {
+	        checkState = '';
+	        $(this).parent().parent().parent().removeClass('selected');
+	        countChecked();
+	    });
 
-$("#buttonsHeader button").click(function() {
-    var id = $(this).attr("id");
-    $("#wrapperDiv div").css("display", "none");
-    $("#wrapperDiv div#" + id + "").css("display", "block");
-    $("#wrapperDiv div#" + id + "").css("visibility", "visible");
-    
-    $("#wrapperDiv div#" + id + " div#displayDropDownRegion").css("display", "inline-block");
-    $("#wrapperDiv div#" + id + " div#displayDropDownRegion").css("visibility", "visible");
-    
-    $("#wrapperDiv div#" + id + " div#displayDropDownState").css("display", "inline-block");
-    $("#wrapperDiv div#" + id + " div#displayDropDownState").css("visibility", "visible");
-});
+	    var checkState = '';
+
+	    $('.bulk_action input').on('ifChecked', function () {
+	        checkState = '';
+	        $(this).parent().parent().parent().addClass('selected');
+	        countChecked();
+	    });
+	    $('.bulk_action input').on('ifUnchecked', function () {
+	        checkState = '';
+	        $(this).parent().parent().parent().removeClass('selected');
+	        countChecked();
+	    });
+	    $('.bulk_action input#check-all').on('ifChecked', function () {
+	        checkState = 'all';
+	        countChecked();
+	    });
+	    $('.bulk_action input#check-all').on('ifUnchecked', function () {
+	        checkState = 'none';
+	        countChecked();
+	    });
+
+	    function countChecked() {
+	        if (checkState === 'all') {
+	            $(".bulk_action input[name='table_records']").iCheck('check');
+	        }
+	        if (checkState === 'none') {
+	            $(".bulk_action input[name='table_records']").iCheck('uncheck');
+	        }
+
+	        var checkCount = $(".bulk_action input[name='table_records']:checked").length;
+
+	        if (checkCount) {
+	            $('.column-title').hide();
+	            $('.bulk-actions').show();
+	            $('.action-cnt').html(checkCount + ' Records Selected');
+	        } else {
+	            $('.column-title').show();
+	            $('.bulk-actions').hide();
+	        }
+	    }  
+	    
+	    $.ajax({
+	    	headers: { 'Content-Type': 'application/json' },
+			url:"/api/main/createAlert", 
+			type:'POST',
+			dataType:'json', 
+			contentType:'application/json',
+			data:JSON.stringify(json)
+		}); 
+	    
+	    
+	    	    
+}
