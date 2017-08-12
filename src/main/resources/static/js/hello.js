d09e190cbff5305e6817fba56b32fcd23070bb4b
urbo143851 <urbo143851@gmail.com>
2017-08-11 19:20:59 -0500

    main controllers

---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,97 @@
+// The name of the application is 'hello'.
+angular.module('hello', [ 'ngRoute' ])
+  .config(function($routeProvider, $httpProvider) {
+
+    $routeProvider.when('/', {
+      templateUrl : 'home.html',
+      controller : 'home',
+      controllerAs: 'controller'
+    }).when('/login', {
+      templateUrl : 'login.html',
+      controller : 'navigation',
+      controllerAs: 'controller'
+    }).otherwise('/');
+
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+  })
+  .controller('home', function($rootScope, $http, $location) {
+    var self = this;   
+  
+    $.getScript("../js/pageActions.js", function(data, textStatus, jqxhr) {
+		console.log('Load was performed for pageActions.js.');
+		});
+    $.getScript("../js/pageSetup.js", function(data, textStatus, jqxhr) {
+		console.log('Load was performed for pageSetup.js.');
+		});
+    
+    self.logout = function() {
+  	  $http.post('logout', {}).finally(function() {
+  	    $rootScope.authenticated = false;
+  	    $location.path("/");
+  	  });
+  	}
+   
+    /*$.getScript("../build/js/custom.js", function(data, textStatus, jqxhr) {
+		console.log('Load was performed for custom.js.');
+		}); */
+  })
+  
+  // navigation controller
+  .controller('navigation',
+		  // 
+		  // 
+		  // 
+		  
+		  function($rootScope, $http, $location) {
+	  		
+	  		
+	  		
+	  	  	
+	  		var self = this;
+	  		
+	  		// local helper function
+	  		var authenticate = function(credentials, callback) {
+	  			
+	  			var headers = credentials ? {authorization : "Basic "
+	  		        + btoa(credentials.username + ":" + credentials.password)
+	  		    	} : {};
+	  			
+	  		    $http.get('user', {headers : headers}).then(function(response) {
+	  		    	if (response.data.name) {
+	  		    		$rootScope.authenticated = true;
+	  		        } else {
+	  		            $rootScope.authenticated = false;
+	  		          }
+	  		        callback && callback();
+	  		        }, function() {
+	  		          $rootScope.authenticated = false;
+	  		          callback && callback();
+	  		        });
+	  		}
+	  
+	  	authenticate();
+	  	self.credentials = {};
+	  	
+	  	self.login = function() {
+	        authenticate(self.credentials, function() {
+	          if ($rootScope.authenticated) {
+	            $location.path("/");
+	            self.error = false;
+	          } else {
+	            $location.path("/login");
+	            $rootscope.showLoginLink=false;
+	            self.error = true;
+	          }
+	        });
+	    };  
+	    
+	    self.logout = function() {
+	    	  $http.post('logout', {}).finally(function() {
+	    	    $rootScope.authenticated = false;
+	    	    $location.path("/");
+	    	  });
+	    	}
+  });
+  
+/*  .controller('etc', function($rootScope, $http, $location) {*/
