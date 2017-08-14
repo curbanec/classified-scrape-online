c519c417400b7df40d0ff5a5e88dc46be89918fa
urbo143851 <urbo143851@gmail.com>
2017-08-13 19:56:00 -0500

    set login page as primary page.

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,24 +1,81 @@
-function login(){
+// The name of the application is 'hello'.
+angular.module('hello', [ 'ngRoute' ])
+  .config(function($routeProvider, $httpProvider) {
+
+    $routeProvider.when('/', {
+      templateUrl : 'home.html',
+      controller : 'home',
+      controllerAs: 'controller'
+    }).when('/login', {
+      templateUrl : 'login.html',
+      controller : 'navigation',
+      controllerAs: 'controller'
+    }).otherwise('/login');
+
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+  })
+  .controller('home', function($rootScope, $http, $location) {
+    var self = this;   
+  
+    $.getScript("../js/pageActions.js", function(data, textStatus, jqxhr) {
+		console.log('Load was performed for pageActions.js.');
+		});
+    $.getScript("../js/pageSetup.js", function(data, textStatus, jqxhr) {
+		console.log('Load was performed for pageSetup.js.');
+		});
     
-        var user = $('#user').val();
-        var pass = $('#pass').val();
-        
-		$.ajax({
-				type:'GET',
-				url:"/api/login/" + user + "/" + pass, 
-				headers: { 'Content-Type': 'application/json' },
-				dataType:'json',
-				contentType:'application/json',
-				statusCode: {
-					200: function(){
-					    console.log("success login");
-					   /* alert("success login");*/
-					    document.cookie = 'username=' + user +'; path=/';
-					    window.location.href = 'home.html';
-	      			},
-					500: function(){
-					    alert("bad login info");
-					}
-				}
-			}); 
-}
+    self.logout = function() {
+  	  $http.post('logout', {}).finally(function() {
+  	    $rootScope.authenticated = false;
+  	    $location.path("/");
+  	  });
+  	}
+  }).controller('navigation',
+
+		  function($rootScope, $http, $location) {
+
+	  		var self = this;
+	  
+	  		// local helper function
+	  		var authenticate = function(credentials, callback) {
+	  			
+	  			var headers = credentials ? {authorization : "Basic "
+	  		        + btoa(credentials.username + ":" + credentials.password)
+	  		    	} : {};
+	  			
+	  		    $http.get('user', {headers : headers}).then(function(response) {
+	  		    	if (response.data.name) {
+	  		    		$rootScope.authenticated = true;
+	  		        } else {
+	  		            $rootScope.authenticated = false;
+	  		          }
+	  		        callback && callback();
+	  		        }, function() {
+	  		          $rootScope.authenticated = false;
+	  		          callback && callback();
+	  		        });
+	  		}
+	  
+	  	authenticate();
+	  	self.credentials = {};
+	  	
+	  	self.login = function() {
+	        authenticate(self.credentials, function() {
+	          if ($rootScope.authenticated) {
+	            $location.path("/");
+	            self.error = false;
+	          } else {
+	            $location.path("/login");
+	            self.error = true;
+	          }
+	        });
+	    };  
+	    
+	    self.logout = function() {
+	    	  $http.post('logout', {}).finally(function() {
+	    	    $rootScope.authenticated = false;
+	    	    $location.path("/");
+	    	  });
+	    	}
+  });
